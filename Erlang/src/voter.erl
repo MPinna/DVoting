2779d1ebd69a654d6acc16491c8e9250e56da991
MPinna <marco.pinn95@gmail.com>
2022-09-05 16:48:43 +0200

    Add function to retrieve voter pub key from id

--- Erlang/src/voter.erl
+++ Erlang/src/voter.erl
@@ -10,7 +10,7 @@
 -author("marco").
 
 %% API
--export([init/0, insert_voter/1, set_voter_flag/1, test_insert/0, select_all_who_voted/0]).
+-export([init/0, insert_voter/1, set_voter_flag/1, get_voter_pub_key_from_id/1, test_insert/0, select_all_who_voted/0]).
 
 %% https://www.erlang.org/doc/man/qlc.html
 %% QLC = Query List Comprehensions
@@ -25,7 +25,6 @@
 %% 4> mnesia:info().
 %%
 init() ->
-  mnesia:start(),
   mnesia:create_table(voter,
                       [{attributes, record_info(fields, voter)}]).
 
@@ -41,13 +40,6 @@ insert_voter(Vot) ->
   % interference
   mnesia:transaction(InsertVoter).
 
-%% delete_voter(Voter_id) ->
-%%
-%%  [V] = mnesia:read(voter, Voter_id, write),
-%%  DeleteVoter = fun() -> mnesia:delete_object(voter, Obj_to_del, write),
-%%  New =
-%%  voter
-
 
 %% Set the has_voted flag of a voter to true
 set_voter_flag(Voter_id) ->
@@ -61,11 +53,6 @@ set_voter_flag(Voter_id) ->
         end,
   mnesia:transaction(Fun).
 
-%%
-%%select_all_voters() ->
-%%  SelectAllVoters = fun() ->
-%%    Voter = #voter{}
-
 
 %% return the list of all the voters with the has_voted flag set to true
 select_all_who_voted() ->
@@ -77,27 +64,37 @@ select_all_who_voted() ->
     },
     mnesia:select(voter, [{HasVoted, [], ['$1', '$2']}])
   end,
-  {atomic,Turnout}=mnesia:transaction(SelectAllWhoVoted),
-  length(Turnout).
+  mnesia:transaction(SelectAllWhoVoted).
+
+%% Get the public key of a voter given their id
+get_voter_pub_key_from_id(Voter_id) ->
+  SelectVoterPubKey = fun() ->
+    Voter = #voter{voter_id = Voter_id,
+      pub_key = '$1',
+      _ = '_'
+    },
+    mnesia:select(voter, [{Voter, [], ['$1']}])
+                       end,
+  mnesia:transaction(SelectVoterPubKey).
 
 test_insert() ->
   Vot1 = #voter{ voter_id = 2,
     name = "Mario",
     surname = "Rossi",
     dob = "1970-1-1",
-    pub_key = "../../resources/v2_key.pem",
+    pub_key = asd,
     has_voted = false},
   Vot2 = #voter{ voter_id = 3,
     name = "Luigi",
     surname = "Verdi",
     dob = "1970-1-2",
-    pub_key = "../../resources/v3_key.pem",
+    pub_key = qwe,
     has_voted = false},
   Vot3 = #voter{ voter_id = 4,
     name = "Wario",
     surname = "Gialli",
     dob = "1970-1-3",
-    pub_key = "../../resources/v4_key.pem",
+    pub_key = zxc,
     has_voted = false},
   insert_voter(Vot1),
   insert_voter(Vot2),
